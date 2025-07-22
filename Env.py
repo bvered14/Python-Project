c83b055bb5e744663b97c1aecf1353132357b162
Sara Bezrukavnikov <sara.bezrukavnikov@gmail.com>
2025-07-22 08:04:14 +0300

    incorporated AA into aggregation, added code for visualization

---
+++ Env.py
@@ -0,0 +1,8 @@
+
+class Environment:
+    def __init__(self, temperature=37, kinase_level=1.0, phosphatase_level=1.0, protease_level=1.0, oxidative_stress=0.0):
+        self.temperature = temperature
+        self.kinase_level = kinase_level
+        self.phosphatase_level = phosphatase_level
+        self.protease_level = protease_level
+        self.oxidative_stress = oxidative_stress

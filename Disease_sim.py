a92666dd00c12f62224830aa8a0cf2def3ab516c
Ilan Peerutin <ipeerutin@gmail.com>
2025-07-22 16:46:17 +0300

    Add files via upload

--- Disease_sim.py
+++ Disease_sim.py
@@ -2,15 +2,17 @@ from AA import AminoAcid
 import Protein
 from typing import Optional, List, Tuple
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 
-def phosporylation_constants(protein: Protein, list_of_PTMs: Optional[List[Tuple[int, str]]] = None):
+def phosphorylation_constants(protein: Protein, list_of_PTMs: Optional[List[Tuple[int, str]]] = None):
     initial_phospo_constant=0.005
     initial_dephospo_constant=0.02
     acetyl_constant_ranges=[2,5,0.8,1]
     methyl_constant_ranges=[0.5,0.9,1,1]
     ubi_constant_ranges=[0.5,1,1.2,2]
     glyco_constant_ranges=[0.1,0.3,1.5,2.5]
-    phospho_residues=0
+    phospho_residues=1
     possible_phopho=0
     acetyl_counts=0
     methyl_counts=0
@@ -46,9 +48,9 @@ def phosporylation_constants(protein: Protein, list_of_PTMs: Optional[List[Tuple
     gdK = random.uniform(glyco_constant_ranges[2], glyco_constant_ranges[3])
     
     pK = initial_phospo_constant * (aK ** acetyl_counts) * (mK ** methyl_counts) * (uK ** ubi_counts) * (gK ** glyco_counts)
-    pdK = initial_dephospo_constant * (adK ** acetyl_counts) * (mdK ** methyl_counts) * (udK ** ubi_counts) * (gdK ** glyco_counts)
+    dpK = initial_dephospo_constant * (adK ** acetyl_counts) * (mdK ** methyl_counts) * (udK ** ubi_counts) * (gdK ** glyco_counts)
 
-    return pK, pdK
+    return pK, dpK
 
 def phosphorylation(protein: Protein, phospho_k, dephospho_k):
     phospho_residues=0
@@ -60,14 +62,21 @@ def phosphorylation(protein: Protein, phospho_k, dephospho_k):
                 possible_phopho+=1
             case __ if aa.three_letter in {"Ser", "Thr", "Tyr"}:
                 possible_phopho+=1
-    dP=phospho_k*(1-(possible_phopho-phospho_residues))-dephospho_k*phospho_residues
+    dP=phospho_k*(1-(phospho_residues/possible_phopho))-dephospho_k*phospho_residues
 
     phospho_residues=0
     possible_phopho=0
     for aa in protein.sequence:
         match aa.PTM:
+            case "Phospho":
+                if random.random()<dephospho_k:
+                    aa.removePTM()
+                    possible_phopho+=1
+                else:
+                    phospho_residues+=1
+                    possible_phopho+=1
             case __ if aa.three_letter in {"Ser", "Thr", "Tyr"}:
-                if random()<dP:
+                if random.random()<dP:
                     aa.addPTM("Phospho")
                     phospho_residues+=1
                     possible_phopho+=1
@@ -75,4 +84,21 @@ def phosphorylation(protein: Protein, phospho_k, dephospho_k):
                     possible_phopho+=1
 
     return phospho_residues/possible_phopho*100
-    
+
+def phospo_over_time(protein: Protein, time: int, list_of_PTMs: Optional[List[Tuple[int, str]]] = None):
+    p_percentage=np.zeros(time)
+    for i in range(time):
+        pK, dpK=phosphorylation_constants(protein,list_of_PTMs)
+        p_percentage[i]=phosphorylation(protein,pK,dpK)
+    return p_percentage
+
+plt.figure(figsize=(10, 6))
+plt.plot(phospho_data, label='Phosphorylation %', color='mediumblue', linewidth=2)
+plt.title('Tau Phosphorylation Simulation Over Time')
+plt.xlabel('Time Steps')
+plt.ylabel('Phosphorylated Residues (%)')
+plt.ylim(0, 100)
+plt.grid(True, linestyle='--', alpha=0.5)
+plt.legend()
+plt.tight_layout()
+plt.show()
